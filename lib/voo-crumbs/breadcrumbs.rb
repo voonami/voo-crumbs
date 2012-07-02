d594dce9bcdceb19d5abb42b9049e220910a8f38
Jason Webb <bigjasonwebb@gmail.com>
2012-07-02 16:14:21 -0600

    Fix dependencies to activesupport instead of all of rails

--- lib/voo-crumbs/breadcrumbs.rb
+++ lib/voo-crumbs/breadcrumbs.rb
@@ -1,3 +1,5 @@
+require 'active_support/concern'
+
 module Voonami
   module Crumbs
     module Breadcrumbs
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,4 @@
 require 'bundler/setup'
-require "rails"
 Bundler.require
 
 Dir["./spec/support/*.rb"].each{|f| require f }
